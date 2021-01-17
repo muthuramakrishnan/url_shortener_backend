c1540e16f8c50ed6a48744a0cb2d73c1a073a5ee
muthuramakrishnan <74167303+muthuramakrishnan@users.noreply.github.com>
2021-01-18 01:10:07 +0530

    Update app.js

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(cors({
-  origin:"https://urlshrinker-mrk.herokuapp.com"
+  origin:"https://mystifying-villani-03a37f.netlify.app"
 }));
 app.use(logger('dev'));
 app.use(express.json());
